c62184fb782648d4edb2fbc4b0aa46886786a378
Nick Chirgin <nickchirgin@gmail.com>
2020-01-27 13:08:15 +0300

    SecondTask

---
+++ app/src/main/java/com/example/androidchirgin/SplashScreenAcitivity.java
@@ -0,0 +1,14 @@
+package com.example.androidchirgin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashScreenAcitivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashscreenactivity);
+    }
+}
